6e02013e101f4f30d01312bd7f3842374337f0e4
tanbin-hasnat-shehab <85411927+tanbin-hasnat-shehab@users.noreply.github.com>
2021-06-20 13:45:37 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 from openpyxl import load_workbook
 
@@ -9,33 +10,8 @@ def about():
 
 
 def main():
-	def caln():
-		section = st.text_input("Section Name")
-		prop = st.text_input("Property Name")
-		section.upper()
-		print(section)
+	
 
-		indi_name=0
-		if section in names:
-			indi_name=names.index(section)+1
-		if section in name_tr_label:
-			indi_name=name_tr_label.index(section)+1
-		
-		
-		
-		for i in range(1,86):
-			if prop==sheet.cell(row=1,column=i).value:
-				indi_prop=i
-				break
-        
-		try:
-			if indi_name!=0:
-				st.write(f'''Value : {sheet.cell(row=indi_name+1,column=indi_prop).value}''')
-			else:
-				st.write('Could not found')
-				
-		except:
-			st.write('Could not found')
 	st.title("Aisc database version - v15 : ")
     
 
@@ -49,16 +25,16 @@ def main():
 		sheet=wb.active
 		names=[]
 
-		for i in range(1,len(sheet['A'])+1):
-			name_tr=sheet.cell(row=i+1,column=2).value
-			names.append(name_tr)
+	
 		name_tr_label=[]
 		for i in range(1,len(sheet['A'])+1):
 			name_tr=sheet.cell(row=i+1,column=3).value
 			name_tr_label.append(name_tr)
-        
+    #######
+    result=st.selectbox('Select or write section',name_tr_label)
+    st.write(f'you selected {result}')
 
-		caln()
+		
         
 
 	elif choice == "About":
